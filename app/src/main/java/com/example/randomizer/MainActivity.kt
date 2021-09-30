809d45843ea1a405538b31a2cd43ca08dae4145e
nicky <nickybsimon@gmail.com>
2021-09-29 23:37:40 -0400

    My App

---
+++ app/src/main/java/com/example/randomizer/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.randomizer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        val totalText = findViewById<TextView>(R.id.TotalText)
+        var total = 0
+        button.setOnClickListener{
+            total += 1
+            totalText.text = total.toString()
+        }
+    }
+}
